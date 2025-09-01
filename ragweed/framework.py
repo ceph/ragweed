a4a005b9fc8a0bd922f91629614aa8430de89ab1
Mark Kogan <mkogan@ibm.com>
2025-09-01 18:23:20 +0000

    ubuntu 24.04 - migrate ragweed from boto to boto3
    boto no longer supported on ubuntu 24.04 as minimal python
    version (without external repo) is 3.12 which requires boto3
    
    Fixes: https://tracker.ceph.com/issues/72500
    
    Signed-off-by: Mark Kogan <mkogan@ibm.com>

--- ragweed/framework.py
+++ ragweed/framework.py
@@ -1,14 +1,13 @@
 import sys
 import os
-import boto
-import boto.s3.connection
+import boto3
+import botocore.exceptions
 import json
 import inspect
 import pickle
 import munch
 import yaml
 import configparser
-from boto.s3.key import Key
 import rados
 
 from .reqs import _make_admin_request
@@ -18,25 +17,282 @@ import pytest
 ragweed_env = None
 suite = None
 
+# Boto3 MultipartUpload compatibility class
+class MultipartUpload:
+    def __init__(self, connection, bucket_name, key_name, upload_id):
+        self.connection = connection
+        self.bucket_name = bucket_name
+        self.key_name = key_name
+        self.id = upload_id
+
+    def upload_part_from_file(self, file_obj, part_num, headers=None):
+        """Upload a part from file object (boto2 compatibility)"""
+        kwargs = {
+            'Bucket': self.bucket_name,
+            'Key': self.key_name,
+            'PartNumber': part_num,
+            'UploadId': self.id,
+            'Body': file_obj.read()
+        }
+        if headers:
+            if 'Content-MD5' in headers:
+                kwargs['ContentMD5'] = headers['Content-MD5']
+
+        response = self.connection.client.upload_part(**kwargs)
+
+        # Return an object with boto2-like attributes
+        part = type('Part', (), {})()
+        part.part_number = part_num
+        part.etag = response['ETag']
+        return part
+
+    def complete_upload(self):
+        """Complete the multipart upload (boto2 compatibility)"""
+        # Get list of parts
+        response = self.connection.client.list_parts(
+            Bucket=self.bucket_name,
+            Key=self.key_name,
+            UploadId=self.id
+        )
+
+        parts = [{'ETag': part['ETag'], 'PartNumber': part['PartNumber']}
+                for part in response['Parts']]
+
+        self.connection.client.complete_multipart_upload(
+            Bucket=self.bucket_name,
+            Key=self.key_name,
+            UploadId=self.id,
+            MultipartUpload={'Parts': parts}
+        )
+
+# Boto3 Key-like wrapper for backward compatibility
+class Key:
+    def __init__(self, bucket):
+        self.bucket = bucket
+        self.key = None
+        self.version_id = None
+        self._metadata = None
+        self._storage_class = None
+
+    def _load_metadata(self):
+        """Load object metadata if not already loaded"""
+        if self._metadata is None and self.key:
+            try:
+                if hasattr(self.bucket, 'connection') and self.bucket.connection:
+                    # BucketWrapper case
+                    response = self.bucket.connection.client.head_object(
+                        Bucket=self.bucket.name,
+                        Key=self.key
+                    )
+                elif hasattr(self.bucket, 'Object'):
+                    # Direct boto3 bucket case
+                    obj = self.bucket.Object(self.key)
+                    response = obj.head()
+                else:
+                    # BucketWrapper case without connection (after deserialization)
+                    print(f"Warning: Cannot load metadata for {self.key} - bucket not properly initialized")
+                    self._metadata = {}
+                    return
+                self._metadata = response
+            except Exception as e:
+                print(f"Warning: Failed to load metadata for {self.key}: {e}")
+                self._metadata = {}
+
+    @property
+    def size(self):
+        """Get object size (boto2 compatibility)"""
+        self._load_metadata()
+        return self._metadata.get('ContentLength', 0)
+
+    @property
+    def etag(self):
+        """Get object ETag (boto2 compatibility)"""
+        self._load_metadata()
+        return self._metadata.get('ETag', '').strip('"')
+
+    @property
+    def name(self):
+        """Get object name (boto2 compatibility)"""
+        return self.key
+
+    @property
+    def storage_class(self):
+        """Get object storage class (boto2 compatibility)"""
+        # Return the explicitly set storage class if available
+        if self._storage_class is not None:
+            return self._storage_class
+        # Otherwise load from metadata
+        self._load_metadata()
+        return self._metadata.get('StorageClass', 'STANDARD')
+
+    @storage_class.setter
+    def storage_class(self, value):
+        """Set object storage class (boto2 compatibility)"""
+        self._storage_class = value
+
+    def set_contents_from_string(self, data, headers=None):
+        kwargs = {'Key': self.key, 'Body': data}
+        if headers:
+            kwargs.update(headers)
+        # Add storage class if set
+        if self._storage_class:
+            kwargs['StorageClass'] = self._storage_class
+        self.bucket.put_object(**kwargs)
+        # Clear metadata cache since object was modified
+        self._metadata = None
+
+    def get_contents_as_string(self):
+        if hasattr(self.bucket, 'Object'):
+            # Direct boto3 bucket case
+            obj = self.bucket.Object(self.key)
+            return obj.get()['Body'].read()
+        else:
+            # BucketWrapper case
+            response = self.bucket.connection.client.get_object(
+                Bucket=self.bucket.name,
+                Key=self.key
+            )
+            return response['Body'].read()
+
 class RGWConnection:
     def __init__(self, access_key, secret_key, host, port, is_secure):
         self.host = host
         self.port = port
         self.is_secure = is_secure
-        self.conn = boto.connect_s3(
-                aws_access_key_id = access_key,
-                aws_secret_access_key = secret_key,
-                host=host,
-                port=port,
-                is_secure=is_secure,
-                calling_format = boto.s3.connection.OrdinaryCallingFormat(),
-                )
+
+        # Construct endpoint URL for boto3
+        protocol = 'https' if is_secure else 'http'
+        endpoint_url = f"{protocol}://{host}:{port}" if port else f"{protocol}://{host}"
+
+        # Create boto3 client and resource
+        self.client = boto3.client(
+            's3',
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            endpoint_url=endpoint_url,
+            use_ssl=is_secure
+        )
+
+        self.resource = boto3.resource(
+            's3',
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            endpoint_url=endpoint_url,
+            use_ssl=is_secure
+        )
+
+        # For backward compatibility, provide conn attribute
+        self.conn = self
 
     def create_bucket(self, name):
-        return self.conn.create_bucket(name)
+        bucket = self.resource.Bucket(name)
+        bucket.create()
+        return BucketWrapper(bucket, self)
 
     def get_bucket(self, name, validate=True):
-        return self.conn.get_bucket(name, validate=validate)
+        bucket = self.resource.Bucket(name)
+        if validate:
+            # Check if bucket exists by trying to get its location
+            try:
+                self.client.head_bucket(Bucket=name)
+            except botocore.exceptions.ClientError as e:
+                error_code = e.response['Error']['Code']
+                if error_code == '404':
+                    raise botocore.exceptions.ClientError(
+                        {'Error': {'Code': 'NoSuchBucket', 'Message': f'The specified bucket does not exist: {name}'}},
+                        'HeadBucket'
+                    )
+                raise
+        return BucketWrapper(bucket, self)
+
+
+class BucketWrapper:
+    """Wrapper to provide boto2-like interface for boto3 bucket"""
+    def __init__(self, bucket, connection):
+        self.bucket = bucket
+        self.connection = connection
+        self.name = bucket.name
+
+    def __getattr__(self, name):
+        # Delegate to underlying bucket for attributes we don't handle
+        return getattr(self.bucket, name)
+
+    def __getstate__(self):
+        # For pickling - exclude non-serializable boto3 objects
+        state = self.__dict__.copy()
+        # Store just the bucket name for reconstruction
+        state['_bucket_name'] = self.name
+        # Remove unpicklable boto3 objects
+        state.pop('bucket', None)
+        state.pop('connection', None)
+        return state
+
+    def __setstate__(self, state):
+        # For unpickling - will need to be reconstructed by framework
+        self.__dict__.update(state)
+        self.bucket = None
+        self.connection = None
+
+    def get_key(self, key_name, validate=False):
+        """Get a key object (boto2 compatibility)"""
+        if validate:
+            try:
+                self.connection.client.head_object(Bucket=self.name, Key=key_name)
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == '404':
+                    return None
+                raise
+
+        # Return a Key-like object
+        key = Key(self)
+        key.key = key_name
+        return key
+
+    def list_multipart_uploads(self):
+        """List multipart uploads (boto2 compatibility)"""
+        response = self.connection.client.list_multipart_uploads(Bucket=self.name)
+        uploads = []
+        for upload in response.get('Uploads', []):
+            # Create a full MultipartUpload object with all methods
+            upload_obj = MultipartUpload(self.connection, self.name, upload['Key'], upload['UploadId'])
+            upload_obj.initiated = upload['Initiated']
+            uploads.append(upload_obj)
+        return uploads
+
+    def initiate_multipart_upload(self, key_name, headers=None):
+        """Initiate multipart upload (boto2 compatibility)"""
+        kwargs = {'Bucket': self.name, 'Key': key_name}
+        if headers:
+            # Map common headers
+            if 'Content-Type' in headers:
+                kwargs['ContentType'] = headers['Content-Type']
+            # Check for storage class header (case-insensitive)
+            if 'x-amz-storage-class' in headers:
+                kwargs['StorageClass'] = headers['x-amz-storage-class']
+            elif 'X-Amz-Storage-Class' in headers:
+                kwargs['StorageClass'] = headers['X-Amz-Storage-Class']
+
+        response = self.connection.client.create_multipart_upload(**kwargs)
+
+        # Return a MultipartUpload object
+        mp = MultipartUpload(self.connection, self.name, key_name, response['UploadId'])
+        return mp
+
+    def configure_versioning(self, versioning):
+        """Configure bucket versioning (boto2 compatibility)"""
+        status = 'Enabled' if versioning == 'true' or versioning is True else 'Suspended'
+        self.connection.client.put_bucket_versioning(
+            Bucket=self.name,
+            VersioningConfiguration={'Status': status}
+        )
+
+    def copy_key(self, new_key_name, src_bucket_name, src_key_name, storage_class=None):
+        """Copy key (boto2 compatibility)"""
+        copy_source = {'Bucket': src_bucket_name, 'Key': src_key_name}
+        kwargs = {'CopySource': copy_source, 'Bucket': self.name, 'Key': new_key_name}
+        if storage_class:
+            kwargs['StorageClass'] = storage_class
+        self.connection.client.copy_object(**kwargs)
 
 
 class RGWRESTAdmin:
@@ -46,7 +302,10 @@ class RGWRESTAdmin:
     def get_resource(self, path, params):
         r = _make_admin_request(self.conn, "GET", path, params)
         if r.status != 200:
-            raise boto.exception.S3ResponseError(r.status, r.reason)
+            raise botocore.exceptions.ClientError(
+                {'Error': {'Code': str(r.status), 'Message': r.reason}},
+                'AdminRequest'
+            )
         return munch.munchify(json.loads(r.read()))
 
 
@@ -105,14 +364,13 @@ class RSuite:
         self.rtests.append(t)
 
     def write_test_data(self, test):
-        k = Key(self.config_bucket)
-        k.key = 'tests/' + test._name
-        k.set_contents_from_string(test.to_json())
+        key_name = 'tests/' + test._name
+        self.config_bucket.put_object(Key=key_name, Body=test.to_json())
 
     def read_test_data(self, test):
-        k = Key(self.config_bucket)
-        k.key = 'tests/' + test._name
-        s = k.get_contents_as_string()
+        key_name = 'tests/' + test._name
+        obj = self.config_bucket.Object(key_name)
+        s = obj.get()['Body'].read().decode('utf-8')
         print('read_test_data=', s)
         test.from_json(s)
 
@@ -158,6 +416,24 @@ class RBucket:
             self.placement_target = self.zone.get_placement_target(self.bucket_info.placement_rule)
         except:
             pass
+
+    def __getstate__(self):
+        # For pickling - exclude non-serializable objects
+        state = self.__dict__.copy()
+        # Store bucket name instead of bucket object
+        state['_bucket_name'] = self.name
+        # Remove unpicklable objects
+        state.pop('zone', None)
+        state.pop('bucket', None)
+        state.pop('placement_target', None)
+        return state
+
+    def __setstate__(self, state):
+        # For unpickling - will need to be reconstructed by framework
+        self.__dict__.update(state)
+        self.zone = None
+        self.bucket = None
+        self.placement_target = None
 
     def get_data_pool(self):
         try:
@@ -256,7 +532,7 @@ class RZone:
         return None
 
     def get_default_placement(self):
-        return get_placement_target(self.zone_params.default_placement)
+        return self.get_placement_target(self.zone_params.default_placement)
 
     def create_bucket(self, name):
         bucket = self.create_raw_bucket(name)
@@ -277,8 +553,26 @@ class RZone:
         return self.conn.regular.get_bucket(name)
 
     def refresh_rbucket(self, rbucket):
-        rbucket.bucket = self.get_raw_bucket(rbucket.bucket.name)
-        rbucket.bucket_info = self.rgw_rest_admin.get_bucket_instance_info(rbucket.bucket.name)
+        # Handle case where RBucket was serialized/deserialized and needs reconstruction
+        if rbucket.bucket is None and hasattr(rbucket, '_bucket_name'):
+            # Reconstruct from serialized state
+            rbucket.zone = self
+            rbucket.bucket = self.get_raw_bucket(rbucket._bucket_name)
+            rbucket.bucket_info = self.rgw_rest_admin.get_bucket_instance_info(rbucket._bucket_name)
+            rbucket.name = rbucket._bucket_name
+            # Reconstruct placement info
+            try:
+                rbucket.placement_rule = RPlacementRule(rbucket.bucket_info.placement_rule)
+                rbucket.placement_target = self.get_placement_target(rbucket.bucket_info.placement_rule)
+            except:
+                pass
+        elif hasattr(rbucket.bucket, 'name'):
+            rbucket.bucket = self.get_raw_bucket(rbucket.bucket.name)
+            rbucket.bucket_info = self.rgw_rest_admin.get_bucket_instance_info(rbucket.bucket.name)
+        else:
+            # Old format compatibility
+            rbucket.bucket = self.get_raw_bucket(rbucket.bucket.name)
+            rbucket.bucket_info = self.rgw_rest_admin.get_bucket_instance_info(rbucket.bucket.name)
 
 
 class RTest:
